49c63bd6765443df16223a9b989c16f26b4e459c
Bruno Gomes <bruno.gomes0579@gmail.com>
2024-08-23 01:48:54 -0300

    feat: frontend complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import { FiSearch } from 'react-icons/fi';
+import './styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Teste Projeto</h1>
+    <div className="container">
+      <h1 className ="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input
+          type="text"
+          placeholder="Digite seu CEP..."
+        />
+
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#FFF"/>
+        </button>
+      </div>
+
+      <main className="main">
+        <h2>CEP: 79004543</h2>
+
+        <span>Rua teste algum</span>
+        <span>Complemento teste algum</span>
+        <span>Vila rosa</span>
+        <span>Sao Paulo</span>
+
+      </main>
+
+
+
     </div>
   );
 }
